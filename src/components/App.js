ebf1f49c68e27ef06464ecfe3a054ace7c7b4e37
sahu829 <55076453+sahu829@users.noreply.github.com>
2020-12-03 19:56:04 +0530

    Change

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "../styles/App.css";
 
-const App = () => {
+export default function App() {
   let [count, setCount] = useState(0);
 
   return (
@@ -26,6 +26,6 @@ const App = () => {
       </div>
     </>
   );
-};
+}
 
-export default App;
+//export default App;
